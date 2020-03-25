082d1899c75befab6b0d464e69073da2d832f3aa
Vijaya Aditya <vatadepalli@hotmail.com>
2020-03-25 16:15:05 +0530

    update

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,7 +12,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "../../node_modules/jquery/dist/jquery.min.js"
 import "../../node_modules/popper.js/dist/umd/popper.min.js"
-import "../../node_modules/bootstrap/dist/js/bootstrap.min.js"
+import "../../node_modules/bootstrap/dist/js/bootstrap.js"
 
 import Header from "./header"
 import "./layout.css"
