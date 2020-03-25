edf4f8906564868b030f5f1bc1de9b6799df7fc0
Vijaya Aditya <vatadepalli@hotmail.com>
2020-03-25 16:42:28 +0530

    update

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "../../node_modules/jquery/dist/jquery.min.js"
+import "../../node_modules/jquery/dist/jquery.js"
 import "../../node_modules/popper.js/dist/umd/popper.min.js"
 import "../../node_modules/bootstrap/dist/js/bootstrap.js"
 
