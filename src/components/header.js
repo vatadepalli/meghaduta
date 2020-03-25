74093157f5a412016a8854e16ac4a4e82f4493fc
Vijaya Aditya <vatadepalli@hotmail.com>
2020-03-25 19:03:20 +0530

    update

--- src/components/header.js
+++ src/components/header.js
@@ -7,13 +7,13 @@ const Header = ({ siteTitle }) => (
   <div className="nav-content">
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       
-    <a className="navbar-brand" href="#">{ siteTitle }</a>
+      <a className="navbar-brand" href="#">{ siteTitle }</a>
     
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
       
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
       
     <ul className="navbar-nav ml-auto">
       
@@ -40,8 +40,7 @@ const Header = ({ siteTitle }) => (
 
     </div>
 
-  </nav>
-
+    </nav>
   </div>
 )
 
